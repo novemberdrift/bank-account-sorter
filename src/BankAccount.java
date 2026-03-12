3614602057e3c3e480969790d4e0b033b30a3b2d
ALex_home_laptop <pervoclassnic@gmail.com>
2026-03-12 11:48:52 +0300

    Initial commit: created class BankAccount

---
+++ src/BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount {
+    private final String accountNumber;
+    private final String accountHolder;
+    private final double balance;
+
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolder() { return accountHolder; }
+    public double getBalance() { return balance; }
+
+}
